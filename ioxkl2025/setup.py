73f5a1b4c9e252c378721402ee7a59dd04a64ec1
yoruyonaka <pyteechristine@gmail.com>
2025-06-21 01:53:16 +0800

    graph instance

--- ioxkl2025/setup.py
+++ ioxkl2025/setup.py
@@ -8,7 +8,7 @@ from google.cloud import spanner
 from google.api_core import exceptions
 
 # --- Configuration ---
-INSTANCE_ID = os.environ.get("SPANNER_INSTANCE_ID","instavibe-graph-instance")
+INSTANCE_ID = os.environ.get("SPANNER_INSTANCE_ID","ioxkl2025-graph-instance")
 DATABASE_ID = os.environ.get("SPANNER_DATABASE_ID","graphdb")
 
 PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
